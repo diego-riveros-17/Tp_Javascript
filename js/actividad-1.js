0937e2e112a96d01e900c62b20b5b027e91f4802
unknown <diegoriveros1991@gmail.com>
2026-05-24 21:00:01 -0300

    feat: se finalizo en punto 1

---
+++ js/actividad-1.js
@@ -0,0 +1,8 @@
+const nombreCompleto = "Riveros Diego Fernando";
+let edad = 34;
+let carrera = "Tecnicatura Superior en Desarrollo de Software Multiplataforma";
+let cursaActualmente = "Si";
+
+console.log(
+  `Nombre ${nombreCompleto} edad ${edad} años carrera ${carrera} cursando actualmente ${cursaActualmente}`,
+);
